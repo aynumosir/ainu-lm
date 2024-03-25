25ef85f8602160866866d3c6aef1e44594193193
Ryo Igarashi <n33t5hin@gmail.com>
2024-03-25 15:45:35 +0900

    Remove trailing slash from AIP_MODEL_DIR

--- src/ainu_lm_trainer/models/job_dir.py
+++ src/ainu_lm_trainer/models/job_dir.py
@@ -1,23 +1,27 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from google.cloud import storage
 from google.cloud.storage import Blob
 
 
 class JobDir:
     """
-    GCPに「ディレクトリ」の概念がないので作ったラッパー。
+    A wrapper around GCS blob as they don't have a concept of directories.
     """
 
     __blob: Blob
 
     def __init__(self, job_dir: str) -> None:
+        # AIP_MODEL_DIR has a trailing slash
+        job_dir = job_dir.rstrip("/")
         self.__blob = Blob.from_string(job_dir, client=None)
 
     def resolve(self, name: str) -> JobDir:
         return JobDir(f"gs://{self.__blob.bucket.name}/{self.__blob.name}/{name}")
 
-    def to_blob(self, client: storage.Client) -> Blob:
+    def to_blob(self, client: Optional[storage.Client]) -> Blob:
         blob = Blob.from_string(str(self), client=client)
         return blob
 
