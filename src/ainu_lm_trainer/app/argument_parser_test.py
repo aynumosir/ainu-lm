e87a6c036d4eb4c3d511921b29fd6f5e0d9b872c
Ryo Igarashi <n33t5hin@gmail.com>
2024-03-26 11:46:11 +0900

    Fix testing for argument parser

--- src/ainu_lm_trainer/app/argument_parser_test.py
+++ src/ainu_lm_trainer/app/argument_parser_test.py
@@ -24,31 +24,27 @@ def test_parsing_language_model_training() -> None:
     assert args.hp_tune == "True"
     assert args.num_train_epochs == 20
 
-    assert args.tokenizer_dir.bucket.name == "test"
-    assert args.tokenizer_dir.name == "tokenizer"
-
+    assert str(args.tokenizer_dir) == "/gcs/test/tokenizer"
     assert str(args.output_dir) == "/gcs/test/output_dir"
     assert str(args.logging_dir) == "/gcs/test/logging_dir"
 
 
-def test_parsing_language_model_with_tensorboard() -> None:
+def test_parsing_language_model_with_local_disk() -> None:
     parser = get_argument_parser()
     args = parser.parse_args(
         [
             "language-model",
             "--hp-tune=True",
             "--num-train-epochs=20",
-            "--tokenizer-dir=gs://test/tokenizer",
-            "--output-dir=gs://test/output_dir",
-            "--logging-dir=gs://test/logging_dir",
+            "--tokenizer-dir=/model/tokenizer",
+            "--output-dir=/model/output_dir",
+            "--logging-dir=/model/logging_dir",
         ]
     )
     assert args.task == "language-model"
     assert args.hp_tune == "True"
     assert args.num_train_epochs == 20
 
-    assert args.tokenizer_dir.bucket.name == "test"
-    assert args.tokenizer_dir.name == "tokenizer"
-
-    assert str(args.output_dir) == "/gcs/test/output_dir"
-    assert str(args.logging_dir) == "/gcs/test/logging_dir"
+    assert str(args.tokenizer_dir) == "/model/tokenizer"
+    assert str(args.output_dir) == "/model/output_dir"
+    assert str(args.logging_dir) == "/model/logging_dir"
